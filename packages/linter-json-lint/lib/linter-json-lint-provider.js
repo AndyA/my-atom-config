5aa6c7d032dd2563146779a17fd3d30262597b75
Andy Armstrong <andy@hexten.net>
2018-04-12 20:06:39 +0100

    Snapshot

--- packages/linter-json-lint/lib/linter-json-lint-provider.js
+++ packages/linter-json-lint/lib/linter-json-lint-provider.js
@@ -1,21 +1,20 @@
-"use babel"
+'use babel'
 
 import JSONLint from 'json-lint'
+const { atom } = global
 
 export default {
 
-  name: "json-lint",
+  name: 'json-lint',
 
   grammarScopes: ['source.json'],
 
   scope: 'file',
 
-  lintOnFly: true,
+  lintsOnChange: true,
 
-  lint(textEditor) {
-
+  lint (textEditor) {
     return new Promise((resolve, reject) => {
-
       var lint = JSONLint(textEditor.getText(), {
         comments: atom.config.get('linter-json-lint.allowComments')
       })
@@ -23,14 +22,15 @@ export default {
       const messages = []
 
       if (lint.error) {
-
         let lineIdx = lint.line - 1
         let columnIdx = Math.max(0, lint.character - 1)
         messages.push({
-          type: 'Error',
-          text: lint.error,
-          filePath: textEditor.getPath(),
-          range: [[lineIdx, columnIdx], [lineIdx, columnIdx + 1]]
+          severity: 'error',
+          location: {
+            file: textEditor.getPath(),
+            position: [[lineIdx, columnIdx], [lineIdx, columnIdx + 1]]
+          },
+          description: lint.error
         })
       }
 
